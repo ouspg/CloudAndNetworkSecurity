bf1aa76ead8ef7039ddb516dd36f9a9377c132bc
Lauri Suutari <55877405+lsuutari19@users.noreply.github.com>
2024-04-17 18:51:58 +0300

    removed deploy ingress from deploy.sh

--- 5. Cloud Security/deploy.sh
+++ 5. Cloud Security/deploy.sh
@@ -48,7 +48,6 @@ helm repo update
 # Add new Kubernetes deployments here
 kubectl apply -k deployments/wordpress/
 kubectl apply -f deployments/
-kubectl apply -f deployments/ingress.yaml
 
 # Patch for Kind to make Ingress accessible from host
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
