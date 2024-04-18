f46479b47da9b2c18813fbee8fb87968e5036cb7
Lauri Suutari <55877405+lsuutari19@users.noreply.github.com>
2024-04-18 09:41:46 +0300

    Update access.sh

--- 5. Cloud Security/access.sh
+++ 5. Cloud Security/access.sh
@@ -10,27 +10,27 @@ else
     exit;
 fi
 
-kubectl port-forward service/wordpress 8080:80 > /dev/null
+kubectl port-forward service/wordpress 8080:80 > /dev/null 2>&1 &
 
 export POD_NAME=$(kubectl get pods --namespace default -l "app=flask-webservice" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward $POD_NAME --address 0.0.0.0 1230:3000 > /dev/null
+kubectl port-forward $POD_NAME --address 0.0.0.0 1230:3000 > /dev/null 2>&1 &
 
 export POD_NAME=$(kubectl get pods --namespace default -l "app=health-check" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward $POD_NAME --address 0.0.0.0 1231:80 > /dev/null
+kubectl port-forward $POD_NAME --address 0.0.0.0 1231:80 > /dev/null 2>&1 &
 
 export POD_NAME=$(kubectl get pods --namespace default -l "app=postgresql" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward $POD_NAME --address 0.0.0.0 5432:5432 > /dev/null
+kubectl port-forward $POD_NAME --address 0.0.0.0 5432:5432 > /dev/null 2>&1 &
 
 export POD_NAME=$(kubectl get pods --namespace default -l "app=internal-proxy" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward $POD_NAME --address 0.0.0.0 1232:3000 > /dev/null
+kubectl port-forward $POD_NAME --address 0.0.0.0 1232:3000 > /dev/null 2>&1 &
 
 export POD_NAME=$(kubectl get pods --namespace default -l "app=system-monitor" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward $POD_NAME --address 0.0.0.0 1233:8080 > /dev/null
+kubectl port-forward $POD_NAME --address 0.0.0.0 1233:8080 > /dev/null 2>&1 &
 
 export POD_NAME=$(kubectl get pods --namespace default -l "app=keycloak" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward $POD_NAME --address 0.0.0.0 6006:8080 > /dev/null
+kubectl port-forward $POD_NAME --address 0.0.0.0 6006:8080 > /dev/null 2>&1 &
 
 export POD_NAME=$(kubectl get pods --namespace default -l "app=wordpress" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward $POD_NAME --address 0.0.0.0 80:80 > /dev/null
+kubectl port-forward $POD_NAME --address 0.0.0.0 80:80 > /dev/null 2>&1 &
 
 echo "Laboratory environment ready."
